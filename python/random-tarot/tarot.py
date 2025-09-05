2a601e4ac8fa9733c3fb45306d100d2ea0717bdd
Alina <alina.snakin@oppi.luovi.fi>
2025-09-05 16:43:39 +0300

    Create tarot.py

---
+++ python/random-tarot/tarot.py
@@ -0,0 +1,23 @@
+import json
+import random
+import os
+
+def get_random_tarot_card(filename='tarot.json'):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    filepath = os.path.join(script_dir, filename)
+
+    with open(filepath, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    tarot_cards = data['tarot']
+    random_card = random.choice(tarot_cards)
+
+    return random_card
+
+card = get_random_tarot_card('tarot.json')
+print(" ")
+print(f"NAME: {card['name']}")
+print(f"SUITE: {card['suite']}")
+# print(f"DESC: {card['description']}")
+print(f"SYMBOLISM: {card['interpretation']}")
+print(" ")
